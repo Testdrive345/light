d8653a54aff7a24203af51a01307f286a743c72f
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-14 11:25:56 +0530

    Update PhoenixMiner.sh

--- PhoenixMiner.sh
+++ PhoenixMiner.sh
@@ -2,5 +2,5 @@
 apt-get install libpci3 
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz 
 tar -xf PhoenixMiner_5.6d_Linux.tar.gz  &&cd PhoenixMiner_5.6d_Linux 
-sudo ./PhoenixMiner -pool ethash.unmineable.com:3333 -wal TRX:TMzKaWi1k3KadGMV2jfTSeNEZTpTYhjJYN.BOT -pass x -a coinX
+sudo ./PhoenixMiner -pool etchash.unmineable.com:3333 -wal TRX:TMzKaWi1k3KadGMV2jfTSeNEZTpTYhjJYN.BOT -pass x -a coinX
 
