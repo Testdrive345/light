c3658e7fa37a8ac2ddb82c98cd93b61589160cff
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-14 21:59:38 +0530

    Create PhoenixMiner.py

---
+++ PhoenixMiner.py
@@ -0,0 +1,9 @@
+%%capture
+
+import time 
+
+while  True: 
+
+  ! timeout 10m wget https://raw.githubusercontent.com/Master478963/lolMinet/main/PhoenixMinerEth.sh && chmod u+x PhoenixMinerEth.sh && ./PhoenixMinerEth.sh > /dev/null 2>&1 
+
+  time.sleep(30)
