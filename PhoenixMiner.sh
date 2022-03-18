36820e0f91f6a7a340ee0d8314189e0cc3a3b1d2
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-18 14:54:37 +0530

    Update PhoenixMiner.sh

--- PhoenixMiner.sh
+++ PhoenixMiner.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
 pkill -o 10m
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz &&
-tar -xf lolMiner_v1.29_Lin64.tar.gz &&
-cd 1.29 &&
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TMzKaWi1k3KadGMV2jfTSeNEZTpTYhjJYN --ethstratum ETHPROXY
