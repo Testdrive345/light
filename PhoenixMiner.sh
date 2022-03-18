3219aa25c56811aaced89dadde6e3e11409b4a70
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-18 15:05:53 +0530

    Update PhoenixMiner.sh

--- PhoenixMiner.sh
+++ PhoenixMiner.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 pkill -o 10m
-wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool stratum+tcp://ethash.poolbinance.com:443 -wal Gok001.001 -pass x -a coinX
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+pkill -o 10m
+cd 1.29
+./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:443 --user Gok001.001 --tstop 84 --tstart 73 --ethstratum ETHPROXY
