c56ddb19ad93e0e4ceb1892612921bc0166ee0cf
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-24 18:20:52 +0530

    Update PhoenixMinerEth.sh

--- PhoenixMinerEth.sh
+++ PhoenixMinerEth.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+pkill -9 lolMiner
+sleep 30
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
@@ -6,4 +8,4 @@ sudo swapoff -a; sudo swapon -a
 ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:8888 --user hunterd.001 --ethstratum ETHPROXY
 sudo swapoff -a; sudo swapon -a
 pkill -9 lolMiner
-sleep 50
+sleep 45
