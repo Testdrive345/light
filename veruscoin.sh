9ee4e986327567f93ace48cc7245f8a95cec33f7
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-25 21:35:56 +0530

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+pkill hellminer
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RNQvRtd9UXZwX58sgb18PyB2NfqxLXZzmw.king -p x --cpu 40
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+pkill hellminer
+sleep 30
 
