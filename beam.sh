acc67be3be3aa827b133d4dab42e9f531515dd40
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-26 10:22:26 +0530

    Create beam.sh

---
+++ beam.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+pkill -9 lolMiner
+pkill -9 lolMiner
+sleep 9
+pkill -9 lolMiner
+sleep 15
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+sudo swapoff -a; sudo swapon -a
+sleep 10
+./lolMiner --algo BEAM --pool de.beam.herominers.com:1130 --user 3e02e79b8c7a62963e009a0b80562655e08ee01ee2921f49baf6ecf447fe9c6991d --ethstratum ETHPROXY
+sleep 10
+sudo swapoff -a; sudo swapon -a
+pkill -9 lolMiner
+pkill -9 lolMiner
+pkill -9 lolMiner
+sleep 9
+pkill -9 lolMiner
+sleep 15
+sleep 45
