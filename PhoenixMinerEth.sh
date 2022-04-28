05b42d7599088279b54562ba22f6bee67969a781
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-28 14:01:25 +0530

    Update PhoenixMinerEth.sh

--- PhoenixMinerEth.sh
+++ PhoenixMinerEth.sh
@@ -3,7 +3,7 @@ pkill -9 lolMiner
 pkill -9 lolMiner
 sleep 9
 pkill -9 lolMiner
-sleep 15
+sleep 120
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
