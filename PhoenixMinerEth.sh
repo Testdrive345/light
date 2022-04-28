20b28a6cb101a18646b19789dceb900268955ad4
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-28 14:16:53 +0530

    Update PhoenixMinerEth.sh

--- PhoenixMinerEth.sh
+++ PhoenixMinerEth.sh
@@ -4,6 +4,8 @@ nvidia-smi -caa
 sleep 9
 nvidia-smi -caa
 sleep 10
+sudo rmmod nvidia
+sleep 9
 pkill -9 lolMiner
 sleep 100
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
