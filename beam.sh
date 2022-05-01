c1f54179593901d0e8d2fd96354aae3247202a70
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-01 12:45:41 +0530

    Update beam.sh

--- beam.sh
+++ beam.sh
@@ -9,7 +9,7 @@ tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
 sudo swapoff -a; sudo swapon -a
 sleep 10
-./lolMiner --algo BEAM --pool de.beam.herominers.com:1130 --user 3e02e79b8c7a62963e009a0b80562655e08ee01ee2921f49baf6ecf447fe9c6991d  --tls on
+./lolMiner --coin BEAM-II --pool de.beam.herominers.com:1130 --user 3e02e79b8c7a62963e009a0b80562655e08ee01ee2921f49baf6ecf447fe9c6991d  --tls on
 sleep 10
 sudo swapoff -a; sudo swapon -a
 pkill -9 lolMiner
