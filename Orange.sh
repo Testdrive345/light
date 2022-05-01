b35c8c5170b52d5b44d837df2ed8561f99b0a8f3
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-05-01 21:34:49 +0530

    Update Orange.sh

--- Orange.sh
+++ Orange.sh
@@ -2,12 +2,9 @@ rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
-clear
-
 sudo apt-get install lxde > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-
 sudo apt install apt-transport-https curl > /dev/null 2>&1
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
@@ -17,6 +14,6 @@ echo "===================80%"
 sudo service xrdp start > /dev/null 2>&1
 echo "=======================100%"
 echo XRDP Address:
- curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "==========="
 sleep 9876543210
